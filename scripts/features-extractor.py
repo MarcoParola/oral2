870c99d290d42201c9832a18afe7affbcbca2dd4
federicominniti <federicominniti98@gmail.com>
2023-12-13 20:58:42 +0000

    added comments to the feature-extractor

--- scripts/features-extractor.py
+++ scripts/features-extractor.py
@@ -32,6 +32,7 @@ def feature_before_projection(cfg):
                  sep=';',
                  engine='python')
     
+    # Retrieve images used in the triplet dataset
     to_get=[]
     for i in range(0, len(oral_ranked_dataset)):
         if oral_ranked_dataset.loc[i, "case_id"] not in to_get:
@@ -44,15 +45,18 @@ def feature_before_projection(cfg):
     with open(cfg.features_extractor.original, "r") as f:
         original_dataset = json.load(f)
 
+    # Maintain only images contained in the original dataset 
     original_dataset["images"] = [item for item in original_dataset["images"] if item["id"] in to_get]
     assert len(original_dataset["images"]) <= len(to_get) # in to_get could be present the "NotFound" value
 
     original_dataset["annotations"] = [item for item in original_dataset["annotations"] if item["image_id"] in to_get]
 
+    # Save the dataset in order to maintain only the subset of the dataset needed
     to_get_features_path = cfg.features_extractor.to_get_feature
     #json.dump(original_dataset, open(os.path.join("./data", "to_get_features_dataset.json"), "w"), indent=2)
     json.dump(original_dataset, open(os.path.join(to_get_features_path.split("/")[1], to_get_features_path.split("/")[2]), "w"), indent=2)
 
+    # Get the features from the model
     dataset = OralClassificationDataset(to_get_features_path, img_tranform)
 
     model = model.to('cuda')
@@ -71,9 +75,11 @@ def feature_before_projection(cfg):
         feature = feature.detach()
         features_dataset.append([dataset.get_image_id(i), feature.tolist(), lbl])
 
+    # Save the feature dataset
     features_dataset = pd.DataFrame(features_dataset, columns=['image_id', 'feature', 'type'])
     features_dataset.to_csv(cfg.features_extractor.features_dataset, sep=';', index=False, header=True)
 
+# Useful for PCA
 def feature_after_projection(cfg):
     model = TripletNetModule.load_from_checkpoint(cfg.log.path+cfg.triplet.checkpoint_path)
     model.eval()
